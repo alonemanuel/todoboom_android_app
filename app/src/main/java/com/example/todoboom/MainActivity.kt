6f622fa31513147e97b1a0d38c0a27cf833db35e
Alon Emanuel <alon@alonemanuel.com>
2020-04-07 09:41:48 +0300

    Added image of dice, also used resourceCompat

--- app/src/main/java/com/example/todoboom/MainActivity.kt
+++ app/src/main/java/com/example/todoboom/MainActivity.kt
@@ -3,23 +3,33 @@ package com.example.todoboom
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val createButton: Button = findViewById(R.id.create_button)
+        diceImage = findViewById(R.id.dice_image)
         createButton.setOnClickListener { addToDo() }
     }
 
     private fun addToDo() {
 //        Toast.makeText(this, getString(R.string.todo_creation_toast), Toast.LENGTH_SHORT).show()
-        val todoText: TextView = findViewById(R.id.todo_text)
-        val randomInt = (100..999).random()
-        todoText.text = randomInt.toString() + " tasks left"
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        dice_image.setImageResource(drawableResource)
     }
 }
