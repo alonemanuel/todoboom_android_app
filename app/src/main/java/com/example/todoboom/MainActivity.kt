e821a49735b4305b6d14955a33c585b5931506a0
Alon Emanuel <alon@alonemanuel.com>
2020-04-11 10:39:18 +0300

    Added a click listener to make my button and text field responsive

--- app/src/main/java/com/example/todoboom/MainActivity.kt
+++ app/src/main/java/com/example/todoboom/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.todoboom
 
+import android.content.Context
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,6 +21,43 @@ class MainActivity : AppCompatActivity() {
         diceImage = findViewById(R.id.dice_image)
         createButton.setOnClickListener { addToDo() }
 
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+        findViewById<TextView> (R.id.nickname_text).setOnClickListener{
+            updateNickname(it)
+        }
+
+    }
+
+    private fun updateNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<TextView>(R.id.done_button)
+
+
+        editText.visibility=View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        editText.requestFocus()
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
+
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+
+        nicknameTextView.visibility = View.VISIBLE
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 
     }
 
