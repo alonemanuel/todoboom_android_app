34c5e14c7de5f0cedb29b9fb787133b96d3c2b0b
Alon Emanuel <alon@alonemanuel.com>
2020-04-11 21:22:58 +0300

    Added piet mondrian activity

---
+++ app/src/main/java/com/example/todoboom/PietActivity.kt
@@ -0,0 +1,50 @@
+package com.example.todoboom
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class PietActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_piet)
+        setListeners()
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+
+    private fun setListeners() {
+        val boxOneText: TextView = findViewById(R.id.box_one_text)
+        val boxTwoText: TextView = findViewById(R.id.box_two_text)
+        val boxThreeText: TextView = findViewById(R.id.box_three_text)
+        val boxFourText: TextView = findViewById(R.id.box_four_text)
+        val boxFiveText: TextView = findViewById(R.id.box_five_text)
+        val rootConstraintLayout: View = findViewById(R.id.constraint_layout)
+
+        val clickableViews: List<View> =
+            listOf(
+                boxOneText,
+                boxTwoText,
+                boxThreeText,
+                boxFourText,
+                boxFiveText,
+                rootConstraintLayout
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+}
