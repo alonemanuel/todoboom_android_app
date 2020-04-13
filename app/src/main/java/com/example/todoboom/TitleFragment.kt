c773b3a90b889725a4bc1156ef3a68078ccc0a9e
Alon Emanuel <alon@alonemanuel.com>
2020-04-13 09:42:36 +0300

    Added a fragment

---
+++ app/src/main/java/com/example/todoboom/TitleFragment.kt
@@ -0,0 +1,30 @@
+package com.example.todoboom
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.todoboom.databinding.FragmentTitleBinding
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+            inflater,
+            R.layout.fragment_title,
+            container,
+            false
+        )
+        return binding.root
+    }
+}
+
+
